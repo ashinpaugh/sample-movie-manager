b68c560ce94a697b97dc87be4a6abfdd11c7332a
Austin Shinpaugh <ashinpaugh@gmail.com>
2017-09-21 15:19:46 -0500

    Added user logins. Removed the Review entity. Added the create Movie flow.

---
+++ src/ATS/Bundle/MovieBundle/Controller/AccountController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ashinpaugh
+ * Date: 9/20/17
+ * Time: 6:24 PM
+ */
+
+namespace ATS\Bundle\MovieBundle\Controller;
+
+
+class AccountController
+{
+    
+}
